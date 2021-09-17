e44fcf5591ed74dc3636d48bba3e9c883771741f
meriemMat <am_matouk@esi.dz>
2021-09-17 17:42:39 +0100

    initial commiy

---
+++ template/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SideBar from './components/Menu/SideBar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SideBar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
